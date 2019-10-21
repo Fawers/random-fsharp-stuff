d4f0466cd83f44369780323bed03fc437aed4f0f
Fabricio S Werneck <fabricioswerneck@gmail.com>
2019-10-21 18:17:16 +0300

    commit step 3

--- code.fsx
+++ code.fsx
@@ -1,65 +1,86 @@
-// Great! But there's one thing that's been bugging me.
-// Did you notice we're washing and cleaning dishes that
-// may already be washed or cleaned?
-// Doesn't feel right, eh? Why would we waste water and
-// dish soap to wash and clean everything again?
-// That's why we will ~choose~ which dishes we should wash,
-// and which dishes we should clean.
+// Mm... I'm not happy with our last step.
+// Creating and raising exceptions is soooo object-oriented-ish.
+// What if we create a type that serves as different errors, and
+// return it in case something goes wrong? Yeah, let's do that.
 
 type Dish =
   | CleanDish
   | WashedDish
   | DirtyDish
 
+type DishError =
+  | AlreadyWashed   // if washing a non-dirty dish
+  | AlreadyCleaned  // if cleaning a cleaned dish
+  | MustWashFirst   // if cleaning a dirty dish
 
-// Also, we have to make sure our functions understand that
-// we're not supposed to wash washed dishes, or clean cleaned
-// dishes. Let's create an exception type and raise it!
-exception DishException of string
+// Looks pretty ok to me, except for one thing:
+// we can't make our functions return a Dish or a DishError
+// depending on how things go. But we need to be able to
+// return either one or another. In that case, we can say
+// that our functions will return a result, which can be
+// either a washed or cleaned dish, or a dish error.
+type DishResult =
+  | Ok of Dish
+  | Error of DishError
 
-
-let washDish (dish: Dish): Dish =
+// And adapt our functions.
+let washDish (dish: Dish): DishResult =
   match dish with
-  | DirtyDish -> WashedDish
-  | _         -> raise <| DishException "no need to wash"
+  | DirtyDish   -> Ok WashedDish
+  | WashedDish  -> Error AlreadyWashed
+  | CleanDish   -> Error AlreadyCleaned
 
-let cleanDish (dish: Dish): Dish =
+let cleanDish (dish: Dish): DishResult =
   match dish with
-  | WashedDish  -> CleanDish
-  | DirtyDish   -> raise <| DishException "must wash first"
-  | CleanDish   -> raise <| DishException "no need to clean"
+  | DirtyDish   -> Error MustWashFirst
+  | WashedDish  -> Ok CleanDish
+  | CleanDish   -> Error AlreadyCleaned
 
+// Good and cool! But now we have another problem: composition doesn't work
+// anymore because we can't pass a DishResult as a Dish.
+// One way to solve it is to define washAndCleanDish by hand:
+let uglyWashAndCleanDish dish =
+  match washDish dish with
+  | Ok d          -> cleanDish d
+  | Error _ as e  -> e
+
+// This works... but... ugh.
+// Maybe we can generalize it in such a way that will let us
+// just compose them as we did previously? Let's see...
+// Here we're taking a function that maps Dish to DishResult,
+// then either (a) extracting the Dish from this DishResult and
+// passing it along, or (b) just returning the DishResult containing
+// a DishError.
+// That is, (Dish -> DishResult) (Dish -> DishResult) -> DishResult
+// Unfortunately, we can't use generic types because we still need
+// to match the cases of the DishResults.
+let (>>=) (f: Dish -> DishResult) (g: Dish -> DishResult) (dish: Dish): DishResult =
+  match f dish with
+  | Ok d          -> g d
+  | Error _ as e  -> e
+
+// Now we can happily compose our functions just like we did before,
+// and taking full advantage of currying and partial application:
+let washAndCleanDish =
+  washDish >>= cleanDish // Note that we're not passing the third argument here.
 
 let dishes = [DirtyDish; DirtyDish; WashedDish; DirtyDish; CleanDish]
-let washAndCleanDish = washDish >> cleanDish
 
-// Now if we try to map washAndCleanDish to dishes, we'll get and exception:
-try
-  List.map washAndCleanDish dishes
-with
-| DishException msg -> printfn "DishException: %s" msg; [] // -> we need this so the
-// compiler won't complain about type mismatches
+// And of course, applying washAndCleanDish to our dishes should return the errors
+// and successes, as can be seen running the code below:
+List.map washAndCleanDish dishes
+  |> printfn "%A"
 
-// To achieve our initial result, we're choosing specific dishes to wash and clean.
-// List.choose works as a combined filter and map: you return *Some value* based
-// on a condition; *Nothing* if the condition isn't met.
-let dishesToWash = List.choose (function DirtyDish as d -> Some d | _ -> None) dishes
-// Also, remember 'function' is just a shortcut to 'match' with the last argument.
-let dishesToClean = List.choose (fun d -> match d with WashedDish -> Some d | _ -> None) dishes
+// Fair enough. Finally, let us compose yet another function that will take care
+// of our error cases:
+let onlyCleanDishes (r: DishResult): DishResult =
+  match r with
+  | Ok CleanDish  | Error AlreadyCleaned  -> Ok CleanDish
+  | Ok WashedDish | Error AlreadyWashed   -> cleanDish WashedDish
+  | Ok DirtyDish  | Error MustWashFirst   -> washAndCleanDish DirtyDish
 
-// Now we wash our dirty dishes:
-let washedDishes = List.map washDish dishesToWash
-printfn "%A" washedDishes
+let makeEverythingShine =
+  washAndCleanDish >> onlyCleanDishes
 
-// And clean it all
-let cleanedDishes = List.map cleanDish <| dishesToClean @ washedDishes
-printfn "%A" cleanedDishes
-
-// Finally, we have all our dishes washed and cleaned. Shiny dishes :)
-let allDishesCleaned =
-  dishes
-  |> List.choose (function CleanDish as d -> Some d | _ -> None)
-  |> (@) <| cleanedDishes
-  // Understanding what's happening here is an exercise to the reader.
-
-printfn "%A" allDishesCleaned
+List.map makeEverythingShine dishes
+  |> printfn "%A"
